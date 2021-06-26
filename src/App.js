e601e683091f5c6e0cb0929171a6900d750f25d9
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-06-26 01:23:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { ErrorMessage, Formik, Form as FormikForm, Field } from "formik";
 
 
 //import Form from "./components/form";
-import "Form.css";
+//import "Form.css";
 
 import "./App.css";
 import Main from "./components/form/Main";
